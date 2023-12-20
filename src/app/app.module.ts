9bd26d6f543ff27438780095ee2e856d6db05d16
Tiago Gomes <tidgomes@gmail.com>
2023-12-20 11:46:03 -0300

    implement created components in the App component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { DescriptionComponent } from './description/description.component';
+import { MainComponent } from './main/main.component';
+import { BindingComponent } from './binding/binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    DescriptionComponent,
+    MainComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule
