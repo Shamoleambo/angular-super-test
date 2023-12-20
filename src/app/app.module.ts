1820c6f44e3460902b12ef09bdb27530f70e4556
Tiago Gomes <tidgomes@gmail.com>
2023-12-20 12:17:23 -0300

    test binding knowledge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
@@ -16,7 +17,8 @@ import { BindingComponent } from './binding/binding.component';
     BindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class BindingComponent {
   private readonly name = "Tiago Gomes"
+  public clicked : boolean = false
+  public toggleText : string = "No"
+  public twoBind: string = "two way binding"
+
+  public buttonClick (event : Event) : void {
+    console.log(event)
+    this.clicked = !this.clicked
+    if(this.clicked) {
+      this.toggleText = "Yes"
+    } else {
+      this.toggleText = "No"
+    }
+  }
 
   getName (): string {
     return this.name
